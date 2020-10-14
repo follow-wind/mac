3edc421e7dca69f59de71300d34b73eca257782a
follow-wind <742639144@qq.com>
2020-10-14 17:29:13 +0800

    test

---
+++ test/test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by yf zhou on 2020/10/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
